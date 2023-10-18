79f349aaf4e41d026c0cc6838ec02f99f2121207
Noah Emke <noahremke@gmail.com>
2023-10-18 18:23:15 -0400

    ahhh cors

--- scripts/service.js
+++ scripts/service.js
@@ -1,4 +1,4 @@
-chrome.runtime.onMessage.addListener((message, sender) => {
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     // The callback for runtime.onMessage must return falsy if we're not sending a response
     (async () => {
       if (message.type === 'open_side_panel') {
@@ -10,5 +10,17 @@ chrome.runtime.onMessage.addListener((message, sender) => {
         //   enabled: true
         // });
       }
+      if (message.type == 'start_here'){
+          const title = (await getCurrentTab()).title;
+        sendResponse({'title': title});
+      }
     })();
+    return true;
   });
+
+  async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+  }
---
+++ scripts/side.js
@@ -0,0 +1,27 @@
+let endGoal = "";
+let startPoint = "";
+let startTime = Date.now();
+let endTime = Date.now();
+
+document.querySelector("#startHere").onclick = async () => {
+    const response = await chrome.runtime.sendMessage({ type: 'start_here' }); 
+    console.log(response.title);
+    document.querySelector("#startPoint").textContent = "Start point: " + response.title;
+    startPoint = response.title;
+    startTime = Date.now() - 3000;
+    document.querySelector("#Time").textContent = "Time: We'll figure this out later";
+    endGoal = await getTitle("https://en.wikipedia.org/wiki/Special:Random");
+    document.querySelector("#endGoal").textContent = "End point: " + endGoal;
+
+    // get a random end point and start timer with 5 seconds extra or as soon as focus leaves sidebar again
+}
+
+const getTitle = (url) => {  
+    return fetch(`https://crossorigin.me/${url}`)
+      .then((response) => response.text())
+      .then((html) => {
+        const doc = new DOMParser().parseFromString(html, "text/html");
+        const title = doc.querySelectorAll('title')[0];
+        return title.innerText;
+      });
+  };
